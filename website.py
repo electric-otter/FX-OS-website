d32b091655876b87336eceed6f9affaea2ae5960
electric-otter <krw122081@gmail.com>
2024-12-14 18:40:23 +0000

    Create website.py

---
+++ website.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title('FX OS, the best OS to use for effects, development, gaming, or personal use')
+st.text('FX OS introduces the features you want.')
+st.text('Community support: Our community is helpful')
+st.text('Good for programming: Our community makes the best IDEs')
+st.text('Good antiviruses: Our community makes antiviruses')
